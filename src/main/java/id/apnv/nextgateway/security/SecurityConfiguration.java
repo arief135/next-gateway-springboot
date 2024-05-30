126df71484af2fddfa02b1973e39c17496888483
Arief Pratama <ariefpratama135@gmail.com>
2024-05-30 23:03:51 +0700

    login from app

--- src/main/java/id/apnv/nextgateway/security/SecurityConfiguration.java
+++ src/main/java/id/apnv/nextgateway/security/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package id.apnv.nextgateway.security;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +17,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import id.apnv.nextgateway.entity.UserInfo;
 import id.apnv.nextgateway.service.UserInfoService;
@@ -26,9 +30,19 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+        http
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/auth/**").permitAll()
+                                                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .csrf((c) -> c.disable())
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Arrays.asList("*"));
+                    configuration.setAllowedMethods(Arrays.asList("*"));
+                    configuration.setAllowedHeaders(Arrays.asList("*"));
+                    return configuration;
+
+                }))
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
@@ -44,17 +58,23 @@ public class SecurityConfiguration {
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) {
-                
+
                 Optional<UserInfo> byId = userInfoService.getById(username);
                 if (!byId.isPresent()) {
                     throw new UsernameNotFoundException(username);
                 }
 
                 return User.withUsername(username)
-                    .password(byId.get().getPassword())
-                    .roles()
-                    .build();
+                        .password(byId.get().getPassword())
+                        .roles()
+                        .build();
             }
         };
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
